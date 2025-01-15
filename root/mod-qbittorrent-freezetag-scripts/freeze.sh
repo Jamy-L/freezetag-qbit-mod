19b95c4b9c5ab5e1112115a0b3837688fdfdbcf2
Jamy-L <jamylafenetre@gmail.com>
2025-01-16 00:15:11 +0100

    Add CATEGORY_UMASK

--- root/mod-qbittorrent-freezetag-scripts/freeze.sh
+++ root/mod-qbittorrent-freezetag-scripts/freeze.sh
@@ -3,6 +3,7 @@
 # Read FREEZE_CATEGORY from the environment, default to an empty string if not set
 FREEZE_CATEGORY="${FREEZE_CATEGORY:-}"
 DOCKER_MODS_DEBUG="${DOCKER_MODS_DEBUG:-false}"
+CATEGORY_UMASK="${CATEGORY_UMASK:-}"
 
 # Function for logging with timestamps
 log() {
@@ -35,11 +36,42 @@ if [[ -z "$FREEZE_CATEGORY" ]]; then
     exit 0
 fi
 
+# Function to set permissions according to the umask for files that don't have .ftag extension
+set_permissions() {
+    if [ -d "$1" ]; then # If it's a directory, recursively process it
+        for item in "$1"/*; do
+            [ -e "$item" ] || continue # Skip in case of empty directories
+            set_permissions "$item"
+        done
+    elif [ -f "$1" ] && [[ "$1" != *.ftag ]]; then # Set permission for all files but not .ftag 
+        chmod $(stat --format '%a' "$1") "$1"
+    fi
+}
+
 # Check if the category matches FREEZE_CATEGORY
 if [[ "$category" == "$FREEZE_CATEGORY" ]]; then
     debug_log "Category '$category' matches FREEZE_CATEGORY $FREEZE_CATEGORY. Freezing torrent data..."
     freezetag freeze "$torrent_path"
     debug_log "Freeze operation completed for path: $torrent_path."
+
+
+    # Set the umask for the category if specified
+    if [[ -n "$CATEGORY_UMASK" ]]; then
+        CURRENT_UMASK=$(umask)
+
+        debug_log "Setting umask $CATEGORY_UMASK for path: $torrent_path."
+
+        umask "$CATEGORY_UMASK" 
+        
+        # Recursively find files and apply the set_permissions function
+        export -f set_permissions
+        find "$torrent_path" -type f -exec bash -c 'set_permissions "$0"' {} \;
+        
+        umask "$CURRENT_UMASK" # Restore the original umask
+        debug_log "Restored the original umask: $CURRENT_UMASK."
+    fi
+
+
 else
     debug_log "Category '$category' does NOT match FREEZE_CATEGORY $FREEZE_CATEGORY. Skipping."
 fi
